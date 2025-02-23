859fc18b45ac981f8902c579555c6de3ed37ccb3
JBarnard05 <Jake-Barnard@live.nmit.ac.nz>
2025-02-24 11:21:52 +1300

    Test with numbers and JS keywords

--- index.js
+++ index.js
@@ -2,7 +2,25 @@
 /** multi line text
  */
 
-console.log("Jake Barnard")
-console.log(2+1)
-var x = 4
-console.log(x)
+//console.log("Jake Barnard")
+//console.log(2+1)
+//var x = 4
+//console.log(x)
+
+//ES5
+var a = 2
+
+//ES6
+const b = 3
+let c = 4
+
+//+,-, / and *
+let num1 = 5
+let num2 = 10
+const result1 = num1 * num2
+console.log("result 1 = " + result1)
+
+let num3 = 5
+let num4 = 10
+const result2 = num4 / num3
+console.log("result 2 = " + result2)
